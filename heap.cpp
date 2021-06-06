ce42a403eac9f965042b1fa8459809140cf721b0
Mrityunjoy99 <mrityunjoydey1999@gmail.com>
2021-06-07 01:09:13 +0530

    Visualise your heap insertion and delition

--- heap.cpp
+++ heap.cpp
@@ -3,50 +3,118 @@ using namespace std;
 class myHeap
 {
     vector<int> v;
-    public:
-        void swap(int i)
+    int size;
+    public: myHeap()
+    {
+        size=0;
+    }
+    private: void swap(int i,int j)
+    {
+        int temp = v[i];
+        v[i] = v[j];
+        v[j] = temp;
+    }
+    private: void heapyfyBottomUp(int index)
+    {
+        while(index)
         {
-            int temp = v[i];
-            v[i] = v[(i-1)/2];
-            v[(i-1)/2] = temp;
-        }
-        void heapyfy()
-        {
-            for(int i=v.size()-1;i>=0;i--)
+            if(v[index]>v[getParent(index)])
             {
-                if(v[i]>v[(i-1)/2])
-                    swap(i);
+                // cout<<"\t";
+                // show();
+                printHeap();
+                swap(index,getParent(index));
+                cout<<endl;
             }
+            else
+                break;
+            index = getParent(index);
         }
-        void heapyfyLast(int index)
+        printHeap();
+    }
+    private: int getLeftChiled(int index)
+    {
+        return index*2+1;
+    }
+    private: int getRightChiled(int index)
+    {
+        return index*2+2;
+    }
+    private: int getParent(int index)
+    {
+        return (index-1)/2;
+    }
+    private: void heapyfyTopDown(int index)
+    {
+        printHeap();
+        cout<<endl;
+        int left = getLeftChiled(index);
+        int right = getRightChiled(index);
+        
+        if((left<size)&&(v[index]<v[left]))
         {
-            while(index)
+            swap(index,left);
+            if((right<size)&&(v[index]<v[right]))
             {
-                if(v[index]>v[(index-1)/2])
-                {
-                    cout<<"\t";
-                    show();
-                    swap(index);
-                }
-                else
-                    break;
-                index = (index-1)/2;
+                swap(index,right);
+                heapyfyTopDown(right);
             }
+            heapyfyTopDown(left);
         }
-        void insert(int i)
+        else if((right<size)&&(v[index]<v[right]))
         {
-            v.push_back(i);
-            heapyfyLast(v.size()-1);
-            cout<<i<<"\t";
-            show();
+            swap(index,right);
+            heapyfyTopDown(right);
+        }
+        else return;
+    }
+    private: void printSpace(int i)
+    {
+        while(i--)
+            cout<<" ";
+    }
+    public: void insert(int i)
+    {
+        v.push_back(i);
+        size++;
+        cout<<"Inserting "<<i<<endl;
+        heapyfyBottomUp(v.size()-1);
+        // cout<<i<<"\t";
+        // show();
 
-        }
-        void show()
+    }
+    public: void pop()
+    {
+        swap(0,size-1);
+        v.pop_back();
+        size--;
+        cout<<"Popping ...."<<endl;
+        heapyfyTopDown(0);
+        // show();
+    }
+    public: void show()
+    {
+        for(auto i:v)
+            cout<<i<<" ";
+        cout<<"\tsize : "<<size;
+        cout<<endl;
+    }
+    public: void printHeap()
+    {
+        int height = log2(size)+1,spaceCt,index=0,upperBound;
+        for(int h=height;h>0;h--)
         {
-            for(auto i:v)
-                cout<<i<<" ";
+            spaceCt = h*(h+1)/2;
+            upperBound = min((index+(int)pow(2,(height-h))),size);
+            while(index<upperBound)
+            {
+                printSpace(spaceCt);
+                cout<<v[index++];
+                printSpace(spaceCt);
+            }
             cout<<endl;
         }
+    }
 };
 
 int main()
@@ -57,6 +125,9 @@ int main()
     h.insert(3);
     h.insert(1);
     h.insert(5);
+    h.pop();
+    h.insert(9);
+    // h.printHeap();
     // h.show();
 
 }
